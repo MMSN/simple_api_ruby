4cbe9a3ebe1b607ee7feaad60c4e3f8515cd2c1d
Mateus <mateus.msnascimento@gmail.com>
2021-03-18 13:04:19 -0300

    feat: finish pagination, add seed to populate db with dummy data

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
     def index
-        articles = Article.recent.page(params[:page]).per(params[:per_page])
+        articles = Article.recent.
+            page(params[:page]).
+            per(params[:per_page])
         render json: articles
     end
 
