93d0269249241664a2ca97dae6d06e7d8857e5e9
Mateus <mateus.msnascimento@gmail.com>
2021-03-18 14:50:32 -0300

    refactor: change user model to disable login null

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '#validations' do
+    it 'should validate presence of attributes' do
+      user = build :users
+      expect(user).to be_valid
+    end
+  end
+
 end
